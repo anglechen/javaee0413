75eb71bbff56cba85e3dc29bb36e8ba5cc688637
aubrey <676719570@qq.com>
2019-04-13 15:07:29 +0800

    添加jdbctemplate以及user的dao实现类，以及单元测试类

---
+++ src/main/java/com/test/web/dao/impl/UserDaoImpl.java
@@ -0,0 +1,74 @@
+/**
+ *
+ */
+package com.test.web.dao.impl;
+
+import java.util.List;
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.test.web.dao.UserDao;
+import com.test.web.model.User;
+
+/**
+ * @author aubrey
+ * @date  下午2:33:02
+ * 
+ */
+@Repository
+//@Controller
+//@Service
+//@Component
+public class UserDaoImpl implements UserDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	
+	
+	@Override
+	public void insert(User user) {
+		/*String sql = "insert into user(name,password)"
+				+ " values('"+ user.getName()+"','"+user.getPassword()+"')";
+		*/
+		String sql = "insert into user(name,password) values(?,?)";
+		jdbcTemplate.update(sql, user.getName(),user.getPassword());
+	}
+
+
+	
+	
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see com.test.web.dao.UserDao#update(com.test.web.model.User)
+	 */
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.test.web.dao.UserDao#del(com.test.web.model.User)
+	 */
+	@Override
+	public void del(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.test.web.dao.UserDao#query(com.test.web.model.User)
+	 */
+	@Override
+	public List<User> query(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/com/test/web/dao/impl/UserDaoImplTest.java
@@ -0,0 +1,58 @@
+/**
+ *
+ */
+package com.test.web.dao.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.test.web.dao.UserDao;
+import com.test.web.model.User;
+
+/**
+ * @author aubrey
+ * @date  下午3:00:48
+ * 
+ */
+public class UserDaoImplTest {
+
+	@Test
+	public void testInsert() {
+		BeanFactory beanFactory = 
+				new ClassPathXmlApplicationContext("application.xml");
+//		UserDao userDao = (UserDao) beanFactory.getBean("userDaoImpl");
+		UserDao userDao = new UserDaoImpl();
+		User user = new User();
+		user.setName("张三");
+		user.setPassword("123456");
+		userDao.insert(user);
+	}
+
+	/**
+	 * Test method for {@link com.test.web.dao.impl.UserDaoImpl#update(com.test.web.model.User)}.
+	 */
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.test.web.dao.impl.UserDaoImpl#del(com.test.web.model.User)}.
+	 */
+	@Test
+	public void testDel() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.test.web.dao.impl.UserDaoImpl#query(com.test.web.model.User)}.
+	 */
+	@Test
+	public void testQuery() {
+		fail("Not yet implemented");
+	}
+
+}
